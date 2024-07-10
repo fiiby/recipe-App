0822057cb3a4a215c534b88f2353ff25cdb103f2
unknown <toohanour@gmail.com>
2024-07-10 12:45:17 +0300

    added recipes details

--- jscript/script.js
+++ jscript/script.js
@@ -1,15 +1,18 @@
+// www.themealdb.com/api/json/v1/1/lookup.php?i={id}
 
 let searchInputEl = document.querySelector('.search-input');
 let searchBtnEl = document.querySelector('#search-btn');
 let resultAreaEl = document.querySelector('.result-area');
+let recipeDetails = document.querySelector('.recipe-details');
 
 // Event listener
 searchBtnEl.addEventListener('click', getRecipes);
-resultAreaEl.addEventListenereventListner('click',getDetails);
+resultAreaEl.addEventListener('click',getRecipeDetails);
+recipeDetails.addEventListener('click',closeRecipeDetails);
 
 function getRecipes() {
   let searchTerm = searchInputEl.value.trim();
-  let apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+  let apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`; // this is correct url as it reject the filter one!
 
   // Fetch data and handle response
   fetch(apiUrl)
@@ -39,7 +42,6 @@ function displayRecipes(recipes) {
           </div>
           <div class="card-info">
             <h2>${recipe.strMeal}</h2>  
-            <br>
             <a href="#" class= "recipe-btn" data-id=${recipe.idMeal}>Get Recipe</a>
           </div>
         </div>
@@ -51,3 +53,42 @@ function displayRecipes(recipes) {
     console.error("Error fetching recipes");
   }
 };
+
+function getRecipeDetails(e) {
+  if(e.target.classList.contains('recipe-btn')) {
+    let id =e.target.getAttribute('data-id');
+    let apiUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+ // Fetch data and handle response
+  fetch(apiUrl)
+  .then(response => response.json()) // Parse response as JSON
+  .then(data => {
+    displayRecipeDetails(data); // Call function to process data
+  })
+  .catch(error => {
+    console.error("Error fetching recipes:", error);
+    resultAreaEl.innerHTML = "An error occurred while fetching recipes."; // Inform user
+  });
+}
+  }
+function displayRecipeDetails(recipeItem) {
+ let item = recipeItem.meals[0];
+ recipeDetails.classList.remove('showDetails');
+  console.log(item);
+
+  recipeDetails.innerHTML = ""; //the empty string to clear existing content
+
+  recipeDetails.innerHTML =`
+  <i  class="fa fa-times" aria-hidden="true"></i>
+  <h2>${item.strMeal}</h2>
+  <p>Instructions: </p>
+  <p>${item.strInstructions}</p>
+  <a href="${item.strYoutube}">watch video</a>
+  `
+}
+function closeRecipeDetails(e) {
+  if(e.target.classList.contains('fa-times')) {
+    e.target.parentElement.classList.add('showDetails');
+  };
+
+}
